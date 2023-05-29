2df3dcf9882d71ecbaf6996532479c4922fc1f00
Bryan Lincoln <bryanufg@gmail.com>
2023-05-29 20:52:25 -0300

    feat: upgrade chat ability

--- habla/chat.py
+++ habla/chat.py
@@ -1,4 +1,5 @@
 import argparse
+from colorama import Fore, Style
 import logging
 import os
 import anthropic
@@ -46,6 +47,7 @@ def main():
         )
         exit(1)
 
+    # greetings!
     print("\n\n")
     typewrite(
         r"""    ___       ___       ___       ___       ___   
@@ -59,32 +61,46 @@ def main():
     )
     print("\n\n")
 
+    # configure the conversation
+    original_message = (
+        anthropic.HUMAN_PROMPT
+        + " You are Hablador, an expert in any kind of software development. Consider this project source:\n"
+        + context
+        + "\n"
+        + "Fulfill this request: "
+    )
+    conversation = [original_message]
+    # chat loop
     while True:
-        user_input = input("You: ")
-        prompt = (
-            anthropic.HUMAN_PROMPT
-            + " You are Hablador, an expert in any kind of software development. Consider this project source:\n"
-            + context
-            + "\n"
-            + "Fulfill this request: "
-            + user_input
-            + anthropic.AI_PROMPT
-        )
-        response = client.completion_stream(
+        user_input = input(f"{Fore.GREEN}You:{Style.RESET_ALL} ").strip()
+        # commands
+        if user_input == "/clear":
+            conversation = [original_message]
+            print("--- Mind cleared. Project context reloaded. ---", flush=True)
+            continue
+
+        # add message to conversation, generate prompt, and get streamed response
+        conversation[-1] += user_input
+        conversation.append(anthropic.AI_PROMPT)
+        prompt = "".join(conversation)
+        stream = client.completion_stream(
             prompt=prompt,
             stop_sequences=[anthropic.HUMAN_PROMPT],
-            max_tokens_to_sample=300,
+            max_tokens_to_sample=400,
             model="claude-instant-v1-100k",
             stream=True,
         )
-        last = ""
-        print("Hablador:", end="", flush=True)
-        for event in response:
+        # print streamed response, token by token
+        print(f"\n{Fore.BLUE}Hablador:{Style.RESET_ALL}", end="", flush=True)
+        response = ""
+        for event in stream:
             intermediate_text = event["completion"]
-            print(intermediate_text[len(last) :], end="", flush=True)
-            last = intermediate_text
+            print(intermediate_text[len(response):], end="", flush=True)
+            response = intermediate_text
         print("\n")
-
+        # configure conversation for next iteration
+        conversation[-1] += response
+        conversation.append(anthropic.HUMAN_PROMPT + " ")
 
 if __name__ == "__main__":
     main()
