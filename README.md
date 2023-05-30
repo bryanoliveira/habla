258aa6d1ac574b7cc57afd4d4cf78a9d0f88fbff
Bryan Lincoln <bryanufg@gmail.com>
2023-05-30 13:19:39 -0300

    feat: improve cli interaction

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
-# Habla CLI
+# Habla
 
-Python 3.8+
+Habla is an assistant that helps you with your software development projects. It scans your codebase and uses AI to answer questions and fulfill requests about it.
+
+## Usage
+
+To use Habla, install it with:
+
+```bash
+pip install git+https://github.com/bryanoliveira/habla --upgrade
+```
+
+Then, run it from any software development project with
+
+```bash
+habla
+```
+
+It will scan your current directory for relevant .py, .js and other text files, and use that context to understand your project.
+
+You can then type in requests, for example:
+
+> Explain the utils.py module
+
+Habla will process your request and use Anthropic's API to generate a relevant response based on the context and request.
+
+You can clear Habla's mind with the /clear command, and it will forget past messages in the current conversation.
+
+## Requirements
+
+- Python 3.8+ 
+- An API key from Anthropic.co to access their AI models.
+- The som Python libraries, installable with pip install -r requirements.txt
+
+---
+This README was generated using `habla` itself!
--- habla/chat.py
+++ habla/chat.py
@@ -1,14 +1,28 @@
 import argparse
+import atexit
 from colorama import Fore, Style
 import logging
+import readline
 import os
+from threading import Thread
+import time
+
 import anthropic
+from rich.console import Console
+from rich.markdown import Markdown
+
 from habla import scanner
 from habla.utils import typewrite
 
 
 LOGLEVEL = os.environ.get("LOGLEVEL", "INFO").upper()
 logging.basicConfig(level=LOGLEVEL)
+
+history_file = os.path.join(os.path.expanduser("~"), ".habla_history")
+readline.read_history_file(history_file)
+readline.parse_and_bind("tab: complete")
+readline.set_history_length(1000)
+atexit.register(readline.write_history_file, history_file)
 
 
 def main():
@@ -27,6 +41,23 @@ def main():
         help="Maximum depth to scan (optional)",
         default=10,
         type=int,
+    )
+    parser.add_argument(
+        "-s", "--stream", help="Stream the assistant response", action="store_true"
+    )
+    parser.add_argument(
+        "-t",
+        "--max-tokens",
+        help="Maximum tokens on the assistant's response",
+        default=400,
+        type=int,
+    )
+    parser.add_argument(
+        "-m",
+        "--model",
+        help="The Anthropic model to use.",
+        default="claude-instant-v1-100k",
+        type=str,
     )
     args = parser.parse_args()
 
@@ -64,15 +95,18 @@ def main():
     # configure the conversation
     original_message = (
         anthropic.HUMAN_PROMPT
-        + " You are Hablador, an expert in any kind of software development. Consider this project source:\n"
+        + " You are Habla, an expert in any kind of software development. Consider this project source:\n"
         + context
         + "\n"
-        + "Fulfill this request: "
+        + "Fulfill this kind request: "
     )
     conversation = [original_message]
     # chat loop
+    console = Console()
     while True:
-        user_input = input(f"{Fore.GREEN}You:{Style.RESET_ALL} ").strip()
+        print(f"\r{Fore.GREEN}You:{Style.RESET_ALL} ", end="", flush=True)
+        user_input = input().strip()
+
         # commands
         if user_input == "/clear":
             conversation = [original_message]
@@ -83,24 +117,51 @@ def main():
         conversation[-1] += user_input
         conversation.append(anthropic.AI_PROMPT)
         prompt = "".join(conversation)
-        stream = client.completion_stream(
+        result = client.completion_stream(
             prompt=prompt,
             stop_sequences=[anthropic.HUMAN_PROMPT],
-            max_tokens_to_sample=400,
-            model="claude-instant-v1-100k",
-            stream=True,
+            max_tokens_to_sample=args.max_tokens,
+            model=args.model,
+            stream=args.stream,
         )
         # print streamed response, token by token
-        print(f"\n{Fore.BLUE}Hablador:{Style.RESET_ALL}", end="", flush=True)
-        response = ""
-        for event in stream:
-            intermediate_text = event["completion"]
-            print(intermediate_text[len(response):], end="", flush=True)
-            response = intermediate_text
+        if args.stream:
+            response = ""
+            print(f"\n{Fore.BLUE}Habla:{Style.RESET_ALL} ", end="", flush=True)
+            for event in result:
+                intermediate_text = event["completion"]
+                print(intermediate_text[len(response) :], end="")
+                response = intermediate_text
+        else:
+            stop_thread = False
+
+            def show_spinning_bar():
+                bar = ["|", "/", "-", "\\"]
+                i = 0
+                while not stop_thread:
+                    print(
+                        f"\r{Fore.BLUE}Habla:{Style.RESET_ALL} (Thinking... {bar[i % 4]})",
+                        end="",
+                        flush=True,
+                    )
+                    time.sleep(0.1)
+                    i += 1
+
+            # think
+            print("")
+            bar_thread = Thread(target=show_spinning_bar)
+            bar_thread.start()
+            response = next(result)["completion"]
+            stop_thread = True
+            bar_thread.join()
+            # respond
+            print(f"\r{Fore.BLUE}Habla:{Style.RESET_ALL}  ", end="", flush=True)
+            console.print(Markdown(response))
         print("\n")
         # configure conversation for next iteration
         conversation[-1] += response
         conversation.append(anthropic.HUMAN_PROMPT + " ")
+
 
 if __name__ == "__main__":
     main()
